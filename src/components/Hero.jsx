a268b27a1526d5318001386e3e6bd12c14ed267b
ArtofNuel <artofnuel@gmail.com>
2022-12-18 14:31:03 +0100

    Updated Hero Image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,85 @@
-import React from 'react'
-import nuel from '../assets/nuel.jpg'
-import { FiTwitter, FiGithub, FiLinkedin } from 'react-icons/fi'
-import { MdOutlineMarkEmailUnread } from 'react-icons/md'
-import { BsArrowRight } from 'react-icons/bs'
-import { Link } from 'react-scroll'
+import React from "react";
+import nuel from "../assets/nuel.jpg";
+import { FiTwitter, FiGithub, FiLinkedin } from "react-icons/fi";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-scroll";
 
 const Hero = () => {
-    return (
-        <div name='home' className='relative max-w-6xl h-screen flex flex-col w-full justify-center items-center mx-auto'>
-            <div className='grid sm:grid-cols-2 justify-center items-center w-full mx-auto'>
-                {/* Container */}
-                <div className="w-full p-4 lg:p-0 text-primary mx-auto flex flex-col justify-center items-start h-full">
-                    <p className='text-xl flex items-center'><span className='w-12 h-1 bg-primary inline-block mr-2'></span>Hello there! My name is</p>
-                    <h1 className='text-5xl md:text-7xl text-primary ml-[-4px]'>Emmanuel Inua</h1>
-                    <h2 className='text-xl'>I'm a Frontend Developer.<br /> I create responsive websites with amazing technologies.</h2>
-                    <Link className='flex justify-center items-center mt-4 w-52 h-[45px] border-2 border-primary text-primary hover:bg-primary hover:text-white duration-500' to="work" smooth={true} duration={500}>
-                        View my work
-                        <BsArrowRight className='ml-3' size={30} />
-                    </Link>
-                </div>
-                <div className='w-full flex justify-center items-center p-4 '>
-                    <img className='w-[400px] rounded border-[10px] border-primary' src={nuel} alt="" />
-                </div>
-            </div>
-            <div className='lg:hidden p-4 absolute bottom-12 flex justify-start items-center space-x-5 w-full pt-5'>
-                <span className='w-12 h-1 bg-primary mr-2 hidden lg:inline-block'></span>
-                <div className='flex justify-center items-center space-x-5 w-full'>
-                    <a className='hover:text-primary md:text-gray' target="_blank" href="https://www.twitter.com/artofnuel">
-                        <FiTwitter size={30} />
-                    </a>
-                    <a className='hover:text-primary md:text-gray' target="_blank" href="https://www.linkedin.com/in/emmanuelinua">
-                        <FiLinkedin size={30} />
-                    </a>
-                    <a className='hover:text-primary md:text-gray' target="_blank" href="https://www.github.com/artofnuel">
-                        <FiGithub size={30} />
-                    </a>
-                    <a className='hover:text-primary md:text-gray' target="_blank" href="mailto:artofnuel@gmail.com">
-                        <MdOutlineMarkEmailUnread size={30} />
-                    </a>
-                </div>
-            </div>
+  return (
+    <main
+      name="home"
+      className="relative max-w-6xl h-screen flex flex-col w-full justify-center items-center mx-auto"
+    >
+      <div className="grid sm:grid-cols-2 justify-center items-center w-full mx-auto">
+        {/* Container */}
+        <div className="w-full p-4 lg:p-0 text-primary mx-auto flex flex-col justify-center items-center h-full">
+          <div>
+            <p className="text-xl flex items-center">
+              <span className="w-12 h-1 bg-primary inline-block mr-2"></span>
+              Hello there! My name is
+            </p>
+            <h1 className="text-5xl md:text-7xl text-primary ml-[-4px]">
+              Emmanuel Inua
+            </h1>
+            <h2 className="text-xl">
+              I'm a Frontend Developer.
+              <br /> I create responsive websites with amazing technologies.
+            </h2>
+            <Link
+              className="flex justify-center items-center mt-4 w-52 h-[45px] border-2 border-primary text-primary hover:bg-primary hover:text-white duration-500"
+              to="work"
+              smooth={true}
+              duration={500}
+            >
+              View my work
+              <BsArrowRight className="ml-3" size={30} />
+            </Link>
+          </div>
         </div>
-    )
-}
+        <div className="w-full flex justify-center items-center p-4 ">
+          <img
+            className="w-[400px] rounded border-[10px] border-primary"
+            src={nuel}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="lg:hidden p-4 absolute bottom-12 flex justify-start items-center space-x-5 w-full pt-5">
+        <span className="w-12 h-1 bg-primary mr-2 hidden lg:inline-block"></span>
+        <div className="flex justify-center items-center space-x-5 w-full">
+          <a
+            className="hover:text-primary md:text-gray"
+            target="_blank"
+            href="https://www.twitter.com/artofnuel"
+          >
+            <FiTwitter size={30} />
+          </a>
+          <a
+            className="hover:text-primary md:text-gray"
+            target="_blank"
+            href="https://www.linkedin.com/in/emmanuelinua"
+          >
+            <FiLinkedin size={30} />
+          </a>
+          <a
+            className="hover:text-primary md:text-gray"
+            target="_blank"
+            href="https://www.github.com/artofnuel"
+          >
+            <FiGithub size={30} />
+          </a>
+          <a
+            className="hover:text-primary md:text-gray"
+            target="_blank"
+            href="mailto:artofnuel@gmail.com"
+          >
+            <MdOutlineMarkEmailUnread size={30} />
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,120 +1,145 @@
-import React, { useState } from 'react'
-import { HiMenuAlt3 } from 'react-icons/hi'
-import { VscCloseAll } from 'react-icons/vsc'
-import { FiTwitter, FiGithub, FiLinkedin } from 'react-icons/fi'
-import { MdOutlineMarkEmailUnread } from 'react-icons/md'
-import { Link } from 'react-scroll'
+import React, { useState } from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { VscCloseAll } from "react-icons/vsc";
+import { FiTwitter, FiGithub, FiLinkedin } from "react-icons/fi";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleNav = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
 
-    return (
-        <div className=''>
-            {/* menu */}
-            <div className='fixed z-30 top-0 w-full h-[50px]'>
-                <div className="backdrop-blur-[2px] bg-secondary/75 flex justify-between items-center w-11/12 md:max-w-7xl mx-auto py-2">
-                    <h1 className='text-3xl'>Emmanuel Inua</h1>
-                    <ul className='hidden lg:flex space-x-6 text-xl'>
-                        <li className='cursor-pointer hover:text-primary' href="/">
-                            <Link to="home" smooth={true} duration={500}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='cursor-pointer hover:text-primary' href="/">
-                            <Link to="about" smooth={true} duration={500}>
-                                About
-                            </Link>
-                        </li>
-                        <li className='cursor-pointer hover:text-primary' href="/">
-                            <Link to="skills" smooth={true} duration={500}>
-                                Skills
-                            </Link>
-                        </li>
-                        <li className='cursor-pointer hover:text-primary' href="/">
-                            <Link to="work" smooth={true} duration={500}>
-                                Work
-                            </Link>
-                        </li>
-                        <li className='cursor-pointer hover:text-primary' href="/">
-                            <Link to="contact" smooth={true} duration={500}>
-                                Contact
-                            </Link>
-                        </li>
-                        <li className='cursor-pointer hover:text-primary' href="/">
-                            <a target="_blank" href="https://artofnuel.mypixieset.com/">ArtofNuel?</a>
-                        </li>
-                    </ul>
-                    {/* Hamburger */}
-                    <div onClick={handleNav} className='lg:hidden z-10'>
-                        {!nav ? <HiMenuAlt3 size={30} /> : <VscCloseAll size={30} />}
-                    </div>
-                </div>
-
-                {/* mobile menu */}
-                <ul className={!nav ? 'hidden' : 'lg:hidden absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center mx-auto space-y-10 text-3xl bg-background'}>
-                    <li className='cursor-pointer hover:text-primary' href="/">
-                        <Link onClick={handleNav} to="home" smooth={true} duration={500}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='cursor-pointer hover:text-primary' href="/">
-                        <Link onClick={handleNav} to="about" smooth={true} duration={500}>
-                            About
-                        </Link>
-                    </li>
-                    <li className='cursor-pointer hover:text-primary' href="/">
-                        <Link onClick={handleNav} to="skills" smooth={true} duration={500}>
-                            Skills
-                        </Link>
-                    </li>
-                    <li className='cursor-pointer hover:text-primary' href="/">
-                        <Link onClick={handleNav} to="work" smooth={true} duration={500}>
-                            Work
-                        </Link>
-                    </li>
-                    <li className='cursor-pointer hover:text-primary' href="/">
-                        <Link onClick={handleNav} to="contact" smooth={true} duration={500}>
-                            Contact
-                        </Link>
-                    </li>
-                    <li className='cursor-pointer hover:text-primary' href="/">
-                        <a target="_blank" href="https://artofnuel.mypixieset.com/">ArtofNuel?</a>
-                    </li>
-                </ul>
-                {/* social icons */}
-                <div className='hidden fixed lg:flex flex-col top-[35%] left-0'>
-                    <ul className='flex flex-col space-y-2'>
-                        <li className='w-[165px] p-1 px-5 flex justify-center items-center ml-[-100px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full'>
-                            <a className='w-full flex justify-between items-center text-primary' target="_blank" href="https://www.twitter.com/artofnuel">
-                                Twitter
-                                <FiTwitter size={30} />
-                            </a>
-                        </li>
-                        <li className='w-[165px] p-1 px-5 flex justify-center items-center ml-[-100px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full'>
-                            <a className='w-full flex justify-between items-center text-primary' target="_blank" href="https://www.linkedin.com/in/emmanuelinua">
-                                Linkedin
-                                <FiLinkedin size={30} />
-                            </a>
-                        </li>
-                        <li className='w-[165px] p-1 px-5 flex justify-center items-center ml-[-100px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full'>
-                            <a className='w-full flex justify-between items-center text-primary' target="_blank" href="https://www.github.com/artofnuel">
-                                Github
-                                <FiGithub size={30} />
-                            </a>
-                        </li>
-                        <li className='w-[165px] p-1 px-5 flex justify-center items-center ml-[-100px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full'>
-                            <a className='w-full flex justify-between items-center text-primary' target="_blank" href="mailto:artofnuel@gmail.com">
-                                Email
-                                <MdOutlineMarkEmailUnread size={30} />
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </div>
+  return (
+    <div className="">
+      {/* menu */}
+      <div className="fixed z-30 top-0 w-full h-[50px]">
+        <div className="backdrop-blur-[2px] bg-secondary/75 flex justify-between items-center w-11/12 md:max-w-7xl mx-auto py-2">
+          <h1 className="text-3xl">Emmanuel Inua</h1>
+          <ul className="hidden lg:flex space-x-6 text-xl">
+            <li className="cursor-pointer hover:text-primary" href="/">
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li className="cursor-pointer hover:text-primary" href="/">
+              <Link to="about" smooth={true} duration={500}>
+                About
+              </Link>
+            </li>
+            <li className="cursor-pointer hover:text-primary" href="/">
+              <Link to="skills" smooth={true} duration={500}>
+                Skills
+              </Link>
+            </li>
+            <li className="cursor-pointer hover:text-primary" href="/">
+              <Link to="work" smooth={true} duration={500}>
+                Work
+              </Link>
+            </li>
+            <li className="cursor-pointer hover:text-primary" href="/">
+              <Link to="contact" smooth={true} duration={500}>
+                Contact
+              </Link>
+            </li>
+            <li className="cursor-pointer hover:text-primary" href="/">
+              <a target="_blank" href="https://artofnuel.mypixieset.com/">
+                ArtofNuel?
+              </a>
+            </li>
+          </ul>
+          {/* Hamburger */}
+          <div onClick={handleNav} className="lg:hidden z-10">
+            {!nav ? <HiMenuAlt3 size={30} /> : <VscCloseAll size={30} />}
+          </div>
         </div>
-    )
-}
 
-export default Navbar
+        {/* mobile menu */}
+        <ul
+          className={
+            !nav
+              ? "hidden"
+              : "lg:hidden absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center mx-auto space-y-10 text-3xl bg-background"
+          }
+        >
+          <li className="cursor-pointer hover:text-primary" href="/">
+            <Link onClick={handleNav} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="cursor-pointer hover:text-primary" href="/">
+            <Link onClick={handleNav} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className="cursor-pointer hover:text-primary" href="/">
+            <Link onClick={handleNav} to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li className="cursor-pointer hover:text-primary" href="/">
+            <Link onClick={handleNav} to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className="cursor-pointer hover:text-primary" href="/">
+            <Link onClick={handleNav} to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+          <li className="cursor-pointer hover:text-primary" href="/">
+            <a target="_blank" href="https://artofnuel.mypixieset.com/">
+              ArtofNuel?
+            </a>
+          </li>
+        </ul>
+        {/* social icons */}
+        <div className="hidden fixed lg:flex flex-col top-[35%] left-0">
+          <ul className="flex flex-col space-y-2">
+            <li className="w-[140px] p-1 px-5 flex justify-center items-center ml-[-75px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full">
+              <a
+                className="w-full flex justify-between items-center text-primary"
+                target="_blank"
+                href="https://www.twitter.com/artofnuel"
+              >
+                Twitter
+                <FiTwitter size={30} />
+              </a>
+            </li>
+            <li className="w-[150px] p-1 px-5 flex justify-center items-center ml-[-85px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full">
+              <a
+                className="w-full flex justify-between items-center text-primary"
+                target="_blank"
+                href="https://www.linkedin.com/in/emmanuelinua"
+              >
+                Linkedin
+                <FiLinkedin size={30} />
+              </a>
+            </li>
+            <li className="w-[140px] p-1 px-5 flex justify-center items-center ml-[-75px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full">
+              <a
+                className="w-full flex justify-between items-center text-primary"
+                target="_blank"
+                href="https://www.github.com/artofnuel"
+              >
+                Github
+                <FiGithub size={30} />
+              </a>
+            </li>
+            <li className="w-[140px] p-1 px-5 flex justify-center items-center ml-[-75px] hover:ml-[-5px] duration-500 border-2 border-primary  rounded-r-full">
+              <a
+                className="w-full flex justify-between items-center text-primary"
+                target="_blank"
+                href="mailto:artofnuel@gmail.com"
+              >
+                Email
+                <MdOutlineMarkEmailUnread size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
